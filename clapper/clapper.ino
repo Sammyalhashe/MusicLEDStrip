700f3ae62f319f1b239d227b89bbe703c53d84b1
Sammyalhashe <sammy.alhashemi@mail.utoronto.ca>
2019-11-16 20:36:37 -0500

    updating algorithm for determining beats

--- clapper/clapper.ino
+++ clapper/clapper.ino
@@ -41,7 +41,13 @@ float volume;
 float maxVol = 15;
 float avgVolume = 0;
 float prevVolume = 0;
+// the average "difference" that would be large enough to cause a trigger
+// I'm thinking about using this to set brightness
+float avgBump = 0;
 int counter = 0; // used for updating the average (number you divide by)
+int bumpCounter = 0;
+float percentage;
+int brightness = BRIGHTNESS;
 // keep track of time the program is active for cool timing affects
 // right now I'm incrementing it by DELAY
 int time = 0;
@@ -79,6 +85,17 @@ void randomColor(Color *c) {
     setColor(c, random8(), 255, random8());
 }
 
+void setNextColor(Color *c, char palette[]) {
+    switch(palette) {
+    case "random" :
+        randomColor(c);
+        break;
+    default       :
+        randomColor(c);
+        break;
+    }
+}
+
 /*
     This method takes a color value and
     inserts its details into an equivalent
@@ -88,6 +105,12 @@ void crgbFromColor(CRGB *crgb, Color c) {
     // CRGB PURPLE = CHSV( HUE_PURPLE, 255, 255 );
     *crgb = CHSV(c.r, c.g, c.b);
 }
+
+void simpleUpdateAverage(float *average, float newVal) {
+    float old = *average;
+    *average = (old + newVal) / 2.0;
+}
+
 void updateAverage(float *average, float newVal, int count) {
     float old = *average;
     *average = old + (newVal - old)/(min(count, FACTOR));
@@ -107,7 +130,7 @@ void setup() {
         as well as the number of LEDs on the strip
     */
     FastLED.addLeds<LED_TYPE, DATAPIN>(leds, NUM_LEDS);
-    FastLED.setBrightness(BRIGHTNESS);
+    FastLED.setBrightness(brightness);
 
     // set an initial color value of white
     setColor(&c, 0, 0, 0);
@@ -140,14 +163,30 @@ void loop() {
         volume = 0.0;
     } else {
         // update the average volume only if there is a meaningful volume
-        counter += 1;
-        updateAverage(&avgVolume, volume, counter);
+        /* counter += 1; */
+        /* updateAverage(&avgVolume, volume, counter); */
+        simpleUpdateAverage(&avgVolume, volume);
         /* avgVolume = (avgVolume + volume) / 2.0; */
     }
 
-    if (volume > THRESH) {
+    // update the maximum recorded volume accordingly
+    if (volume > maxVol) maxVol = volume;
+
+    // if there is a significant volume change
+    // we only consider when the prevVolume is at most at the avgVolume level
+    if (volume - prevVolume > avgVolume - prevVolume && avgVolume - prevVolume > 0) {
+        // update avgBump
+        /* bumpCounter += 1; */
+        /* updateAverage(&avgBump, volume - prevVolume, bumpCounter); */
+        simpleUpdateAverage(&avgBump, volume - prevVolume);
+        // calulate the percentage of the avgBump relative to the avgVolume
+        // this percentage is then used to translate into a brightness value
+        // between 0-255 (basically percentage * 255)
+        percentage = avgBump / avgVolume;
+        brightness = (int) (percentage * 255);
+        FastLED.setBrightness(brightness);
         // write a random rgb(hsv) color into this Color variable
-        randomColor(&c);
+        setNextColor(&c, "");
         // taking that color, translate it to CRGB value and store it in crgb
         crgbFromColor(&crgb, c);
 
